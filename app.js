591d8f5a94ee489098abee1f68c86e5f0c8b583a
unknown <electromenn@gmail.com>
2021-10-18 15:18:55 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+
+const app = express();
+
+app.use('view engine', 'ejs');
+
+app.get("/", function(req, res) {
+    var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
+
+    if (currentDay === 6 || currentDay === 0) {
+        day === "Weekend";
+        res.sendFile(__dirname + "/weekend.html");
+    } else {
+        day === "Weekday";
+        res.sendFile(__dirname + "/weekday.html");
+    }
+});
+
+app.listen(3000, function (){
+    console.log("Server started on port 3000.")
+})
