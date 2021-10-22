660c004164f54b777287eb9bf5a82f5c67aa305f
unknown <electromenn@gmail.com>
2021-10-22 12:10:45 +0300

    friday 12:10

--- app.js
+++ app.js
@@ -3,50 +3,36 @@ const bodyParser = require("body-parser")
 
 const app = express();
 
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res) {
 
-    var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
-
-    // if (currentDay === 6 || currentDay === 0) {
-    //     day = "Weekend";
-    // } else {
-    //     day = "Weekday";
-    // }
-
-    switch (currentDay) {
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        case 0:
-            day = "Sunday";
-            break;
+    let today = new Date();
     
-        default:
-            console.log("Error, there's no more than 7 days in a week lol. Current day is esqual to" + currentDay)
-            break;
-    }
+    let options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    };
+
+    let day = today.toLocaleDateString("en-US", options);
    
-    res.render("list", {kindOfDay: day});
+    res.render("list", {kindOfDay: day, newListItems: items});
 });
+
+app.post("/", function(req, res){
+
+    let item = req.body.newItem;
+
+    items.push(item);
+    
+    res.redirect("/");  
+})
 
 
 app.listen(3000, function (){
